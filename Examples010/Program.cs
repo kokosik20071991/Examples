246fc9619b6c109523213c091a9a9c8f7ddfddb0
Viktoria <vikatim4encko@yandex.ru>
2022-12-09 22:29:30 +0300

    Example010 поиск позиции нужного элемента

---
+++ Examples010/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {1, 12, 31, 18, 15, 16, 17, 18, 19};
+int m = array.Length;
+int find=18;
+int index =0;
+while (index < m) 
+{
+    if (array[index]==find)
+{
+    Console.WriteLine(index);
+break;
+}
+index++;
+}
