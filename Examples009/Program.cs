e1a543b2e3fb1ecde09ffcd53406a09e830e8e94
Viktoria <vikatim4encko@yandex.ru>
2022-12-09 17:48:22 +0300

    Функция и массив недоделанная

---
+++ Examples009/Program.cs
@@ -0,0 +1,12 @@
+﻿int Max (int arg1, int arg2, int arg3)
+{
+   int result = arg1;
+    if(arg2>result) result = arg2;
+    if(arg3>result) result = arg3;
+    return result;
+}
+
+int[] array = { 11, 22, 83, 63, 67, 18, 54, 42, 37};
+array[0] = 12;
+Console.WriteLine(array[0]);
+
